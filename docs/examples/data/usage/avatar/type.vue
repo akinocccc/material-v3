afa6a51ddd2b759865c67219086b8e279ba4b6f5
akinocccc <akinochen@foxmail.com>
2023-04-15 22:25:54 +0800

    docs(avatar): add `MdAvatar` component's document

---
+++ docs/examples/data/usage/avatar/type.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <md-avatar
+      src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+      size="small"
+    />
+    <md-avatar
+      src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+      size="default"
+    />
+    <md-avatar
+      src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+      size="large"
+    />
+    <md-avatar size="small"> A </md-avatar>
+    <md-avatar> A </md-avatar>
+    <md-avatar size="large"> A </md-avatar>
+  </div>
+</template>
