2aad7e09c151bfac39775d258fff3abcde0cd417
akinocccc <akinochen@foxmail.com>
2023-04-22 16:51:30 +0800

    docs(components): [avatar] add document

---
+++ docs/components/data/usage/avatar/type.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-grid align="center" :col-gap="10">
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="small"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="default"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="large"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar size="small"> A </md-avatar>
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar> A </md-avatar>
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar size="large"> A </md-avatar>
+    </md-grid-item>
+  </md-grid>
+</template>
