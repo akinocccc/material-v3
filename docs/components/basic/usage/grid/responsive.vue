60e38c49f00b2cb0a31a0ff508bca74a3b21b8bc
akinocccc <akinochen@foxmail.com>
2023-04-22 16:51:13 +0800

    docs(components): [grid] add document

---
+++ docs/components/basic/usage/grid/responsive.vue
@@ -0,0 +1,35 @@
+<template>
+  <md-grid class="grid-layout" :row-gap="20" :col-gap="20">
+    <md-grid-item class="item" :xs="4" :sm="3" :md="4" :lg="2" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="4" :sm="4" :md="8" :lg="10" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="4" :sm="2" :md="6" :lg="6" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="12" :sm="3" :md="2" :lg="6" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="4" :sm="2" :md="4" :lg="8" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="8" :sm="3" :md="4" :lg="4" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="8" :sm="2" :md="2" :lg="4" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="4" :sm="5" :md="6" :lg="4" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+    <md-grid-item class="item" :xs="12" :sm="12" :md="12" :lg="4" :xl="12">
+      <div class="grid-content">item</div>
+    </md-grid-item>
+  </md-grid>
+</template>
+
+<style lang="scss" scoped>
+@use "./style.scss";
+</style>
