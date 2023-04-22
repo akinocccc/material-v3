a73360fcf7d107a215e5cafbe2eefad439ae8dc3
akinocccc <akinochen@foxmail.com>
2023-04-22 16:51:30 +0800

    docs(components): [avatar] add document

---
+++ docs/components/data/usage/avatar/shape.vue
@@ -0,0 +1,46 @@
+<template>
+  <md-grid align="center" :col-gap="10">
+    <!-- Circle -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="circle"
+        size="small"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="circle"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="circle"
+        size="large"
+      />
+    </md-grid-item>
+    <!-- Square -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="square"
+        size="small"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="square"
+      />
+    </md-grid-item>
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        shape="square"
+        size="large"
+      />
+    </md-grid-item>
+  </md-grid>
+</template>
---
+++ docs/components/data/usage/avatar/size.vue
@@ -0,0 +1,32 @@
+<template>
+  <md-grid align="center" :col-gap="10">
+    <!-- Small size. -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="small"
+      />
+    </md-grid-item>
+    <!-- Defalut size. -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="default"
+      />
+    </md-grid-item>
+    <!-- Large size. -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        size="large"
+      />
+    </md-grid-item>
+    <!-- Customizable size. -->
+    <md-grid-item>
+      <md-avatar
+        src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+        :size="100"
+      />
+    </md-grid-item>
+  </md-grid>
+</template>
